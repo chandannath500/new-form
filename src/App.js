ffd280ef8e71385e093ce45aca2d257d06b91ea0
Chandan Nath <46443346+chandannath500@users.noreply.github.com>
2023-08-23 15:37:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Button } from 'antd';
+import FormPage from './components/FormPage';
+import TablePage from './components/TablePage';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [formModalVisible, setFormModalVisible] = useState(false);
+  const [formDataList, setFormDataList] = useState([]);
+  const [showCreateFormButton, setShowCreateFormButton] = useState(true);
+
+  const handleFormSubmit = formData => {
+    setFormDataList([...formDataList, formData]);
+    localStorage.setItem('formData', JSON.stringify([...formDataList, formData]));
+    setFormModalVisible(false);
+    setShowCreateFormButton(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          background: 'black',
+          padding: '10px 20px',
+          color: 'white',
+        }}
+      >
+        <div style={{ fontWeight: 'bold', fontSize: '18px' }}>EazyTaxes</div>
+        {showCreateFormButton && (
+          <Button
+            style={{
+              fontSize: '18px',
+              border: 'none',
+              borderRadius: '5px',
+              fontWeight: 'bold',
+              paddingBottom: '15px',
+              background:"black",
+              color:'white'
+
+            }}
+            onClick={() => setFormModalVisible(true)}
+          >
+            Create Form
+          </Button>
+        )}
+      </nav>
+      <FormPage
+        visible={formModalVisible}
+        onClose={() => setFormModalVisible(false)}
+        onSubmit={handleFormSubmit}
+      />
+      {formDataList.length > 0 && <TablePage formDataList={formDataList} />}
     </div>
   );
-}
+};
 
 export default App;
